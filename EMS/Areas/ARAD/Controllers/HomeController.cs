dd864447ec5a5edafa327411e9b72077f7339358
cloudshift0 <cloudshiftgro@gmail.com>
2025-07-12 22:06:23 +0100

    Add Areas And template

---
+++ EMS/Areas/ARAD/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Areas.ARAD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EMS/Areas/ARFM/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Areas.ARFM.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
